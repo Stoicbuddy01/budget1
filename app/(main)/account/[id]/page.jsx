7edfe1db265d2253c16aa415ff27b0492846585c
Adrija Sengupta <137429634+Adrija05@users.noreply.github.com>
2025-03-16 02:37:15 +0530

    fetch,render,change default account

---
+++ app/(main)/account/[id]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AccountsPage= () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default AccountsPage
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import  CreateAccountDrawer  from "@/components/create-account-drawer";
 import { Card, CardContent } from '@/components/ui/card';
 import { Plus } from 'lucide-react';
+import { getUserAccounts } from '@/actions/dashboard';
+import AccountCard from './_components/account-card';
 
-function DashboardPage ()  {
+async function DashboardPage ()  {
+    const accounts = await getUserAccounts();
+
     return (
         <div className="px-5">
             {/* budget Progress  */}
@@ -22,6 +26,10 @@ function DashboardPage ()  {
                 </Card>
                 </CreateAccountDrawer>
             
+            {accounts.length > 0 && accounts?.map((account) => {
+                return <AccountCard key={account.id} account={account}/>;
+            })}
+
             </div>
 
         </div>
