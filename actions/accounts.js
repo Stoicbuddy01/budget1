ae6038095ad0bcb6c0a229fd00994fc5b25f10eb
Adrija Sengupta <137429634+Adrija05@users.noreply.github.com>
2025-03-16 12:18:12 +0530

    Account page

--- actions/accounts.js
+++ actions/accounts.js
@@ -3,6 +3,18 @@ import {auth} from "@clerk/nextjs/server";
 import { db } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
+const serializeDecimal = (obj) => {
+  const serialized = { ...obj };
+  if (obj.balance) {
+    serialized.balance = obj.balance.toNumber();
+  }
+  if (obj.amount) {
+    serialized.amount = obj.amount.toNumber();
+  }
+  return serialized;
+};
+
+
 const serializeTransactions = (obj) => {
     const serialized = { ...obj };
     if (obj.balance) {
@@ -42,4 +54,37 @@ const serializeTransactions = (obj) => {
     }catch(error){
         return {success:false , error:error.message};
     }
+  }
+
+  export async function getAccountWithTransactions(accountId) {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+  
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+  
+    if (!user) throw new Error("User not found");
+  
+    const account = await db.account.findUnique({
+      where: {
+        id: accountId,
+        userId: user.id,
+      },
+      include: {
+        transactions: {
+          orderBy: { date: "desc" },
+        },
+        _count: {
+          select: { transactions: true },
+        },
+      },
+    });
+  
+    if (!account) return null;
+  
+    return {
+      ...serializeDecimal(account),
+      transactions: account.transactions.map(serializeDecimal),
+    };
   }
--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -1,9 +1,40 @@
+import { notFound } from 'next/navigation';
 import React from 'react'
+import { getAccountWithTransactions } from '@/actions/accounts';
 
-const AccountsPage= () => {
+const AccountsPage = async ({ params }) => {
+  const { id } = await params; // Await params before accessing its properties
+  const accountData = await getAccountWithTransactions(id);
+
+  if(!accountData){
+    notFound();
+  }
+
+  const {transaction,...account}=accountData;
+
   return (
-    <div>page</div>
+    <div className="space-y-8 px-5">
+      <div className="flex gap-4 items-end justify-between">
+        <div>
+          <h1 className="text-5xl sm:text-6xl font-bold tracking-tight gradient-title capitalize">
+            {account.name}
+          </h1>
+          <p className="text-muted-foreground">
+            {account.type.charAt(0) + account.type.slice(1).toLowerCase()}{" "}
+            Account
+          </p>
+        </div>
+  
+        <div className="text-right pb-2">
+          <div className="text-xl sm:text-2xl font-bold">
+            ${parseFloat(account.balance).toFixed(2)}
+          </div>
+          <p className="text-sm text-muted-foreground">
+            {account._count.transactions} Transactions
+          </p>
+        </div>
+      </div>
+    </div>  
   )
-}
-
+} 
 export default AccountsPage
