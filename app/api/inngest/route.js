55b9b7f251e7e47b50e63d4fa21002061a8a37ed
akshjswl <jaiswal.aksh2005@gmail.com>
2025-03-18 22:49:03 +0530

    Revert "Cron Job for Budget Alert"
    This reverts commit 42667a1d4675d8bfa01f9c00f6279194c62a713f.

--- app/api/inngest/route.js
+++
@@ -1,8 +0,0 @@
-import { serve } from "inngest/next";
-import { inngest } from "../../../ingest/client";
-
-// Create an API that serves zero functions
-export const { GET, POST, PUT } = serve({
-    client: inngest,
-    functions: [checkBudgetAlert],
-});
