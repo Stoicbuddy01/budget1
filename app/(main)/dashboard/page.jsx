287b6d929a34d5d8bb47c750b85614f424e4db38
Stoicbuddy01 <ankitsahaopen@gmail.com>
2025-03-21 23:56:34 +0530

    Dashboard pie chart

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import  CreateAccountDrawer  from "@/components/create-account-drawer";
 import { Card, CardContent } from '@/components/ui/card';
 import { Plus } from 'lucide-react';
-import { getUserAccounts } from '@/actions/dashboard';
+import { getDashboardData, getUserAccounts } from '@/actions/dashboard';
 import AccountCard from './_components/account-card';
 import { getCurrentBudget } from '@/actions/budget';
 import BudgetProgress from './_components/budget-progress';
+import { DashboardOverview } from './_components/transaction-overview';
 
 async function DashboardPage ()  {
     const accounts = await getUserAccounts();
@@ -15,6 +16,8 @@ async function DashboardPage ()  {
     if (defaultAccount) {
         budgetData = await getCurrentBudget(defaultAccount.id);
     }
+
+    const transaction = await getDashboardData();
     return (
         <div className="space-y-8">
             {/* budget Progress  */}
@@ -26,6 +29,12 @@ async function DashboardPage ()  {
 
 
             {/* overview */}
+            <Suspense fallback={"Loading Overview..."}>
+                <DashboardOverview
+                accounts={accounts}
+                transactions={transaction || []}
+                />
+            </Suspense>
 
             {/* Account Grid */}
             <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
