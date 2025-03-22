941f0b3fd3ae026f97c732231f3fcd6bbc8eba27
Stoicbuddy01 <ankitsahaopen@gmail.com>
2025-03-22 21:04:15 +0530

    BOT PROTECTION

--- middleware.js
+++ middleware.js
@@ -1,18 +1,36 @@
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute=createRouteMatcher([
   "/dashboard(.*)",
   "/account (.*)",
   "/transaction(.*)",
-])
-export default clerkMiddleware(async(auth,req)=>{
+]);
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"]
+    }),
+  ],
+});
+
+const clerk = clerkMiddleware(async(auth,req)=>{
   const {userId}=await auth();
 
   if(!userId && isProtectedRoute(req)){
     const {redirectToSignIn}=await auth();
+
     return redirectToSignIn();
   }
 });
+
+export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
